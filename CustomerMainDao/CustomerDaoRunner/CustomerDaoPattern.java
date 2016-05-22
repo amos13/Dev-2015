6122f2fe84a18e04b1dcf055b82adff9883caff0
amos13 <amos2013@outlook.com>
2016-05-22 09:37:50 +0200

    First Constructor's comment

--- CustomerMainDao/CustomerDaoRunner/CustomerDaoPattern.java
+++ CustomerMainDao/CustomerDaoRunner/CustomerDaoPattern.java
@@ -5,6 +5,7 @@ import CustomerDaoImpl.CustomerDaoImpl;
 
 public class CustomerDaoPattern {
 
+	//A static string to put inside the constructor
 	private static final String TXT_TAGNAME_ELEMENT = "customer";
 	
 	public static void main(String[] args) throws CustomerDaoDataException
